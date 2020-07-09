16f849a4e8beccf239bd21339d3f8acb2b39bf19
Michael Bohan <mbohan@tnsk.com>
2020-07-09 13:45:42 -0500

    Initial Github Commit

--- examples/setup_cppyy.py
+++
@@ -1,8 +0,0 @@
-import cppyy
-import os
-olddir = os.getcwd()
-os.chdir("..")
-#cppyy.add_include_path("deps/opendnp3/cpp/lib/include/")
-cppyy.load_library("deps/opendnp3/build/cpp/lib/libopendnp3.so")
-cppyy.include("AllHeaders.h")
-os.chdir(olddir)
